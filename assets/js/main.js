9d28d4200adf04eafd722e3909980c4dc2ee5c2e
Siyoung September <loldruger@gmail.com>
2025-03-18 08:23:44 +0900

    WIP

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,17 @@
 const foldingCircles = document.querySelectorAll('.folding-circle');
 
 const main = () => {
-    foldingCircles.forEach(circle => {
+    for (let circle of foldingCircles) {
         circle.addEventListener('click', () => {
+            const contentBody = circle.parentElement.parentElement.querySelectorAll('.content-body');
             circle.classList.toggle('folded');
+
+            for (let sibling of contentBody) {
+                sibling.classList.toggle('rolled-up');
+            }
         });
-    });
+    }
+
 }
 
 main();

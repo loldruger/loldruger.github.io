f2ab084b14e931717ada78c6035abb21d793ce48
Siyoung September <loldruger@gmail.com>
2025-03-19 10:56:47 +0900

    add toggle button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const main = () => {
     const foldingCircles = document.querySelectorAll('.folding-circle');
-    
+    const toggleButtons = document.querySelectorAll('.toggle-button');
+
     for (const circle of foldingCircles) {
         const contentList = circle.parentElement.parentElement.querySelector('.content-list');
 
@@ -11,6 +12,13 @@ const main = () => {
                 sibling.classList.toggle('rolled-up');
             }
   
+        });
+    }
+
+    for (const button of toggleButtons) {
+        button.addEventListener('click', () => {
+            const circle = button.querySelector('svg > circle');
+            circle.classList.toggle('active');
         });
     }
 }
